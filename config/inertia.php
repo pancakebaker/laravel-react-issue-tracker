c254531281230ef56bdc7c0f6cbf7d810591ab37
Emil J T <emil.john.tele@gmail.com>
2026-08-19 23:03:55 +0800

    fix: configure Inertia test page path

---
+++ config/inertia.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'ensure_pages_exist' => false,
+
+    'page_paths' => [
+        resource_path('js/pages'),
+    ],
+
+    'page_extensions' => [
+        'js',
+        'jsx',
+        'svelte',
+        'ts',
+        'tsx',
+        'vue',
+    ],
+
+    'testing' => [
+        'ensure_pages_exist' => true,
+
+        'page_paths' => [
+            resource_path('js/pages'),
+        ],
+
+        'page_extensions' => [
+            'js',
+            'jsx',
+            'svelte',
+            'ts',
+            'tsx',
+            'vue',
+        ],
+    ],
+
+];
